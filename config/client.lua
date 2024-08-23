16989afc0e324983d1dbbd1b0d302445732925b0
justrowbie <rowbieplaying@gmail.com>
2024-08-24 02:47:44 +0300

    update locales and config spawn

--- config/client.lua
+++ config/client.lua
@@ -1,15 +1,7 @@
 return {
     spawns = {
         {
-            label = 'Legion Square',
-            coords = vec4(195.17, -933.77, 29.7, 144.5)
-        },
-        {
-            label = 'Paleto Bay',
-            coords = vec4(80.35, 6424.12, 31.67, 45.5),
-        },
-        {
-            label = 'Motels',
+            label = 'Kos Rama',
             coords = vec4(324.08, -213.90, 54.09, 165.50),
         },
     }
